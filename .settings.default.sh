e2531040daf23c02ab6cb628420827ec75d19e0f
Michael Morisy <m@morisy.com>
2017-04-08 11:32:56 -0400

    Create .settings.default.sh
    Hoping that a default settings file makes the process clearer as well as provides documentation on how to get various API keys.

---
+++ .settings.default.sh
@@ -0,0 +1,27 @@
+# Copy and save this file as settings.sh and add your API keys into it. MAKE SURE YOU DO NOT CHECK IN API KEYS INTO A COMMIT.
+
+export DOCUMENTCLOUD_USERNAME=""
+export DOCUMENTCLOUD_PASSWORD=""
+export SECRET_KEY=""
+export STRIPE_SECRET_KEY=""
+export STRIPE_PUB_KEY=""
+export MAILGUN_ACCESS_KEY=""
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY="
+
+# Where to get API Keys
+#
+# Document Cloud:
+# https://www.documentcloud.org/plans/apply
+# Note that DocumentCloud has an application process and typically only verified newsrooms will get a key.
+# 
+# Stripe:
+# https://dashboard.stripe.com/account/apikeys
+#
+# Mailgun:
+# https://app.mailgun.com/app/account/security
+# 
+# AWS:
+# https://console.aws.amazon.com/iam/home?region=us-west-2#/security_credential
+# 
+# Need to add Phaxio
